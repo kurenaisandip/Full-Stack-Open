e86ebf76fa4a165e23a6a2745d774dd09c10ab5e
Sandip Rai <srai68141@gmail.com>
2024-04-11 22:38:58 +0545

    finished the react part

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,30 +1,31 @@
-const Hello = () => {
+const Hello = (props) => {
+
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-
 const App = () => {
- const now = new Date();
-  const a = 10;
-  const b = 12;
-  console.log(now, a+b);
+
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-    <p> Hello World</p>
-    <Hello/>
-    <Hello/>
-    <Hello/>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
-
-
   )
 }
 
 export default App;
 
-
+//React cannot render objects instead the individuals things render in the braces must be primtive values, such as numbers or strings.
