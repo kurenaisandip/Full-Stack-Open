2e6f9c28258e03a6d1a402154ac49fe4d5ec1d31
Sandip <dummybaka564@gmail.com>
2024-04-16 20:11:04 +0545

    1.5: Course Information step 5

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,19 +1,22 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-   {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
 
@@ -59,10 +62,10 @@ const App = () => {
 
   return (
     <div>
-    <Header course = {course} />
-    <Content part1={part1} part2={part2} part3={part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
-    <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
-    </div>
+     <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+       </div>
   )
 }
 export default App;
